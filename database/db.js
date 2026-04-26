06e4ea86515d8575b175befffd28092f805c91fd
Ayden05h <ayden.hall@mymail.lourdes.edu>
2026-04-26 11:09:51 -0400

    code fixes

--- database/db.js
+++ database/db.js
@@ -1,13 +1,20 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
-    }
-});
+    })
+    : new Sequelize({
+        dialect: "sqlite",
+        storage: "./database.sqlite"
+    });
 
 module.exports = sequelize;
