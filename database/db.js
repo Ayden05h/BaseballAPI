3415863ba02ff3741d1b04f09047df1e3e6e34d4
Ayden05h <ayden.hall@mymail.lourdes.edu>
2026-04-26 10:56:47 -0400

    Implement JWT authentication and role-based authorization

--- database/db.js
+++ database/db.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./database.sqlite",
-    logging: false 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
-
-sequelize.authenticate()
-    .then(() => console.log("Database connected successfully"))
-    .catch(err => console.error("Database connection error:", err));
 
 module.exports = sequelize;
